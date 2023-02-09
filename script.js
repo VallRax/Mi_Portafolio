be9aadc093bcfbb94df1a1937d9a2724fad69d72
VallRax <101760319+VallRax@users.noreply.github.com>
2023-02-09 20:37:37 -0300

    Portafolio Finalizado, Posibles cambios a futuro

--- script.js
+++ script.js
@@ -23,14 +23,11 @@ function efectoHabilidades(){
         let habilidades = document.getElementsByClassName("progreso");
         habilidades[0].classList.add("javascript");
         habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
-        habilidades[5].classList.add("comunicacion");
-        habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
+        habilidades[2].classList.add("comunicacion");
+        habilidades[3].classList.add("trabajo");
+        habilidades[4].classList.add("creatividad");
+        habilidades[5].classList.add("dedicacion");
+        habilidades[6].classList.add("proyect");
     }
 }
 
